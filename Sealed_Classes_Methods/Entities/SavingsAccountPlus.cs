fac4d156d889c455c6b3600b20d89134afbfec31
Renan Beraldi <beraldi2019renan@gmail.com>
2020-09-09 18:06:34 -0300

    Program Ended

---
+++ Sealed_Classes_Methods/Entities/SavingsAccountPlus.cs
@@ -0,0 +1,11 @@
+﻿namespace Sealed_Classes_Methods.Entities
+{
+    class SavingsAccountPlus : SavingsAccount
+    {   
+        //Este método não pode ser executado pois foi selado no método sobrescrito da classe 'SavingsAccount'
+        public override void Withdraw(double amount)
+        {
+
+        }
+    }
+}
